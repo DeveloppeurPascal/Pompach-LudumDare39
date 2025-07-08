9cb0427dd75939a2c689274df24e82858caef97d
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-07-08 08:23:47 +0200

    updated copyright and timestamp header comments

--- src-dessin-logo/Project1.dpr
+++ src-dessin-logo/Project1.dpr
@@ -1,34 +1,34 @@
-﻿/// <summary>
-/// ***************************************************************************
-///
-/// Pompach
-///
-/// Copyright 2017-2024 Patrick Prémartin under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-/// Patrick PREMARTIN
-///
-/// Site :
-/// https://pompach.gamolf.fr/
-///
-/// Project site :
-/// https://github.com/DeveloppeurPascal/Pompach/
-///
-/// ***************************************************************************
-/// File last update : 2024-09-17T17:59:24.480+02:00
-/// Signature : 0b205728ca74aa0ed90c763b99d158380970aac9
-/// ***************************************************************************
-/// </summary>
+﻿(* C2PP
+  ***************************************************************************
+
+  Pompach
+
+  Copyright 2017-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://pompach.gamolf.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Pompach/
+
+  ***************************************************************************
+  File last update : 2025-07-03T10:44:25.552+02:00
+  Signature : 7b6167be64fdf7f61fa9fd7e1e69dbf5b66829b2
+  ***************************************************************************
+*)
 
 program Project1;
 
--- src-dessin-logo/Unit1.pas
+++ src-dessin-logo/Unit1.pas
@@ -1,34 +1,34 @@
-/// <summary>
-/// ***************************************************************************
-///
-/// Pompach
-///
-/// Copyright 2017-2024 Patrick Prémartin under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-/// Patrick PREMARTIN
-///
-/// Site :
-/// https://pompach.gamolf.fr/
-///
-/// Project site :
-/// https://github.com/DeveloppeurPascal/Pompach/
-///
-/// ***************************************************************************
-/// File last update : 2024-09-17T17:59:24.480+02:00
-/// Signature : a3c3e3f4d33e65df524b1f4833dab71949a6fc64
-/// ***************************************************************************
-/// </summary>
+(* C2PP
+  ***************************************************************************
+
+  Pompach
+
+  Copyright 2017-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://pompach.gamolf.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Pompach/
+
+  ***************************************************************************
+  File last update : 2025-07-03T10:44:25.557+02:00
+  Signature : e7ab8a95a834c9d1c1860f48395bb06249c05acc
+  ***************************************************************************
+*)
 
 unit Unit1;
 
